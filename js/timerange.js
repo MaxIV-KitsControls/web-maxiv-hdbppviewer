e719de37591025f3b469ba2ab8c140512bbbc4f8
Johan Forsberg <johan.forsberg@maxiv.lu.se>
2016-09-13 20:39:59 +0200

    switch timerange widget

--- js/timerange.js
+++ js/timerange.js
@@ -1,165 +1,122 @@
 import React from "react";
 import {findDOMNode} from "react-dom";
 import {connect} from "react-redux";
-import { Button, Input, Glyphicon } from 'react-bootstrap';
+import { ButtonToolbar, Button, Input, Glyphicon } from 'react-bootstrap';
+import { DateRange } from 'react-date-range';
 import moment from 'moment';
 
 import {setTimeRange} from "./actions";
 
 
-// class TimeRange extends React.Component {
-    
-//     constructor () {
-//         super();
-//         this.state = {
-// 	    ranges: {
-// 		'Today': [moment(), moment()],
-// 		'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-// 		'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-// 		'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-// 		'This Month': [moment().startOf('month'), moment().endOf('month')],
-// 		'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-// 	    },
-// 	    startDate: moment().subtract(29, 'days'),
-// 	    endDate: moment()
-// 	}
-//     }
-    
-//     handleEvent (event, picker) {
-// 	this.props.dispatch(setTimeRange(
-// 	    picker.startDate,
-// 	    picker.endDate));
-//     }
-    
-//     render () {
-//         return (
-// 	        <DateRangePicker
-//                     timePicker={true}
-//                     startDate={this.props.startTime}
-//                     endDate={this.props.endTime}
-//                     ranges={this.state.ranges}
-//                     onEvent={this.handleEvent.bind(this)}>
-// 		<Button className="selected-date-range-btn" style={{width:'100%'}}>
-// 		<div className="pull-left"><Glyphicon glyph="calendar" /></div>
-// 		<div className="pull-right">
-// 		<span>
-// 		Time range
-//                 </span>
-// 		<span className="caret"></span>
-// 		</div>
-// 		</Button>
-//                 </DateRangePicker>
-// 	)
-//     }
-// }
+const defaultRanges = {
+    'Today': {
+        startDate: function startDate(now) {
+            return now;
+        },
+        endDate: function endDate(now) {
+            return now;
+        }
+    },
 
+    'Yesterday': {
+        startDate: function startDate(now) {
+            return now.add(-1, 'days');
+        },
+        endDate: function endDate(now) {
+            return now.add(-1, 'days');
+        }
+    },
 
-class _TimeRange2 extends React.Component {
+    'Last 7 Days': {
+        startDate: function startDate(now) {
+            return now.add(-7, 'days');
+        },
+        endDate: function endDate(now) {
+            return now;
+        }
+    },
 
-    render () {
-        return (<table style={{width: "100%"}} className="time-range">
-                  <tbody>
-                    <tr>
-                      <td>
-                        {this.props.startTime.toLocaleString()}
-                </td>
-                <td>
-                <DatetimeRangePicker
-                  timePicker
-                  timePicker24Hour
-                  startDate={moment("2016-07-01")}
-                  endDate={moment("2016-08-01")}>
-
-                <input type="text" value="hej"/>
-                <span className="input-group-btn">
-                    <Button className="default date-range-toggle">
-                      <i className="fa fa-calendar"/>
-                    </Button>
-                </span>                
-                </DatetimeRangePicker>
-                </td>
-                      <td style={{textAlign: "right"}}>
-                        {this.props.endTime.toLocaleString()}
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>);
+    'Last 30 Days': {
+        startDate: function startDate(now) {
+            return now.add(-30, 'days');
+        },
+        endDate: function endDate(now) {
+            return now;
+        }
     }
-    
 }
 
 
-class TimeRange2 extends React.Component {
+class TimeRange extends React.Component {
 
-    componentDidMount() {
-        const element = findDOMNode(this.refs.range);
-        console.log("flepp", element);
-        $(element).daterangepicker(
-            {
-                // timePicker: true,
-                // timePicker24Hour: true,
-                // timePickerIncrement: 60,
-                locale: {
-                    format: 'DD-MM-YYYY',
-                    firstDay: 1
-                },
-                ranges: {
-                    "Today": [moment().subtract(1, 'days'), moment()],
-                    "Yesterday": [moment().subtract(2, 'days'),
-                                  moment().subtract(1, 'days')],
-                    "Last 7 days":  [moment().subtract(6, 'days'), moment()],
-                    'Last 30 Days': [moment().subtract(29, 'days'), moment()]
-                },
-                maxDate: moment(),
-                linkedCalendars: false,
-                drops: "down",
-                opens: "left"
-            },
-            this.onChange.bind(this)
-        );
-        this.picker = $(element).data('daterangepicker');
-    }
-
-    componentWillReceiveProps(props) {
-        console.log("datetime tange", props.timeRange);
-        if (props.timeRange != this.props.timeRange) {
-            this.noChange = true;
-            this.setTimeRange(props.timeRange)
-            this.noChange = false;
+    constructor (props) {
+        super();
+        this.state = {
+            show: false,
+            startDate: moment(props.timeRange.start),
+            endDate: moment(props.timeRange.end)
         }
     }
     
-    onChange(start, end) {
-        console.log("range", start, end)
-        if (!this.noChange)
-            this.props.dispatch(setTimeRange(start._d, end._d))
+    handleSelect (range) {
+	this.setState(range);
     }
 
-    setTimeRange(range) {
-        const {start, end} = range;
-        this.picker.setStartDate(start);
-        this.picker.setEndDate(end);
-        findDOMNode(this.refs.range).innerHTML = this.formatTimeRange();
-    }
-    
-    shouldComponentUpdate() {
-        return false;
+    handleApply() {
+        this.props.dispatch(setTimeRange(this.state.startDate.toDate(),
+                                         this.state.endDate.toDate()));
+        this.setState({show: false})
     }
 
-    formatTimeRange () {
-        const {start, end} = this.props.timeRange;
-        console.log("startTime", start, end)
-        const startDate = start.toLocaleDateString(),
-              endDate = end.toLocaleDateString();
-        if (startDate == endDate)
-            return startDate
-        return `${startDate} - ${endDate}`
+    handleShow() {
+        this.setState({show: !this.state.show})
     }
     
     render () {
-        return <div ref="range">...</div>
-    }
-    
+
+        const startDate = this.props.timeRange.start.toDateString(),
+              endDate = this.props.timeRange.end.toDateString();
+        const dateString = (startDate == endDate?
+                            startDate :
+                            startDate + " - " + endDate);
+        /* TODO: the calendar popup is done in a pretty primitive way,
+           but I could not get the bootstrap Overlay stuff to work with
+           proper positioning. Might be worth looking into at some point. */
+        const dateRange = (
+                <div style={{position: "absolute",
+                             width: "800px", padding: "10px",
+                             borderRadius: "5px", background: "white",
+                             display: this.state.show? "block" : "none",
+                             zIndex: "100",
+                             right: "10px"}}>
+                  <DateRange startDate={moment(this.props.timeRange.start)}
+                             endDate={moment(this.props.timeRange.end)}
+                             onChange={this.handleSelect.bind(this)}
+                             ranges={defaultRanges}
+                             theme= {{Calendar : {width : '300px',
+                                                  padding : '10px 9px'}}}/>
+                  <ButtonToolbar>
+                    <Button bsStyle="success" title="Plot the selected range"
+                            onClick={this.handleApply.bind(this)}>Apply</Button>
+                    <Button bsStyle="danger"
+                            title="Close the calendar withput changes"
+                            onClick={this.handleShow.bind(this)}>Cancel</Button>
+                  </ButtonToolbar>
+            </div>);
+
+        return (<div ref="trigger">
+                  <Button  onClick={this.handleShow.bind(this)}
+                           active={this.state.show}
+                           title="Click to manually select a range of dates">
+                    <span className="pull-left"><Glyphicon glyph="calendar"/>
+                    </span>
+                    <span style={{whiteSpace: "nowrap"}}>
+                      {dateString}
+                    </span>
+                  </Button>
+                  {dateRange}
+                </div>);
+    }    
 }
 
 
@@ -171,4 +128,4 @@ const mapStateToProps = (state) => {
 }
 
 
-export default connect(mapStateToProps)(TimeRange2);
+export default connect(mapStateToProps)(TimeRange);
