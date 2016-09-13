9f42134067daf2fd25d2efef9526308ff35d6cc0
Johan Forsberg <johan.forsberg@maxiv.lu.se>
2016-09-13 11:07:16 +0200

    Clean up UI

--- js/attributes.js
+++ js/attributes.js
@@ -5,7 +5,7 @@ import { bindActionCreators } from 'redux'
 // import AutoSuggest from 'react-autosuggest';
 import fetch from "isomorphic-fetch";
 import { Input, Button, DropdownButton, MenuItem, Col, Panel,
-         Overlay } from 'react-bootstrap';
+         Overlay, FormGroup, FormControl, Modal, Accordion, Table } from 'react-bootstrap';
 
 import * as actionCreators from "./actions";
 import {debounce} from "./utils"
@@ -23,7 +23,8 @@ class Attributes extends React.Component {
             pattern: '',
             suggestions: [],
             selectedSuggestions: [],
-            selectedAttributes: []
+            selectedAttributes: [],
+            showSuggestions: false
         };
     }
 
@@ -50,9 +51,8 @@ class Attributes extends React.Component {
         this.setState({selectedAttributes: selected});
     }
     
-    onAddAttributes (event, axis) {
+    onAddAttributes (axis, event) {
         this.props.addAttributes(this.state.selectedSuggestions, axis);
-        this.setState({selectedSuggestions: []})
     }
 
     onRemoveAttributes () {
@@ -60,60 +60,13 @@ class Attributes extends React.Component {
         this.setState({selectedAttributes: []})
     }
 
-    makeSuggestions() {
-        let suggestOptions = this.state.suggestions.map(sugg => {
-            return <option key={sugg} value={sugg}
-                           disabled={this.props.attributes.indexOf(sugg) != -1}>
-                       {sugg}
-                   </option>;
-        })
-
-        return (
-                <div style={{
-                        ...this.props.style,
-                        position: 'absolute',
-                        backgroundColor: '#EEE',
-                        boxShadow: '0 5px 10px rgba(0, 0, 0, 0.2)',
-                        border: '1px solid #CCC',
-                        borderRadius: 3,
-                        padding: 5}}>
-                <Input type="select" value={this.props.selectedSuggestions}
-                    ref="suggestions" multiple={true}
-                    style={{width: "100%", height: "60%"}}
-                    onChange={this.onSelectSuggestions.bind(this)}>
-                  {suggestOptions}
-                </Input>
-                <DropdownButton title="Add"
-                    disabled={this.state.selectedSuggestions.length == 0}>
-                  <MenuItem eventKey={0}
-                      onSelect={this.onAddAttributes.bind(this)}>Y1</MenuItem>
-                  <MenuItem eventKey={1}
-                      onSelect={this.onAddAttributes.bind(this)}>Y2</MenuItem>
-                </DropdownButton>
-                <Button onClick={this.removeSuggestions.bind(this)}>Close</Button>
-           </div>
-        );
-    }
-
-    removeSuggestions () {
-        this.setState({suggestions: []})
-    }
-
-    componentDidUpdate () {
-        if (this.state.suggestions.length > 0) {
-            const node = findDOMNode(this.refs.suggestions)
-            console.log("hej", node);
-            node.childNodes[0].focus()
-        }
-    }
-    
     // return appropriate content for a select element that
     // shows the current attributes, grouped by Y axis
     getCurrentAttributeOptions () {        
         const axes = Array.from(new Set(
             Object.keys(this.props.config).map(k => this.props.config[k].axis))).sort();
         return axes.map(axis => (
-                <optgroup label={`Y axis ${axis}`}>
+                <optgroup label={axis === 0? "Left Y axis" : "Right Y axis"}>
                 {this.props.attributes
                  .filter(a => this.props.config[a].axis == axis)
                  .map(attr => (
@@ -136,55 +89,84 @@ class Attributes extends React.Component {
             const attr = this.state.selectedAttributes[0];
             const config = this.props.config[attr]
             const desc = this.props.desc[attr]
-            return (<table className="attribute-info">
-                    <tr>
-                    <td>Name:</td> <td>{attr}</td>
-                    </tr>
-                    <tr>
-                    <td>Axis:</td> <td>{config.axis}</td>
-                    </tr>
-                    <tr>
-                    <td>Color:</td> <td>{config.color}</td>
-                    </tr>
-                    <tr>
-                    <td>Points:</td> <td>{desc.total_points}</td>
-                    </tr>
-                    </table>);
+            return (<Table condensed className="attribute-info">
+                      <thead>
+                        <tr>
+                          <th>Name:</th> <th>{attr}</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>Axis:</td> <td>{config.axis}</td>
+                        </tr>
+                        <tr>
+                          <td>Color:</td> <td>{config.color}</td>
+                        </tr>
+                        <tr>
+                          <td>Points:</td> <td>{desc.total_points}</td>
+                        </tr>
+                      </tbody>
+                    </Table>);
         }
     }
     
     render () {
 
-        let attributeOptions = this.getCurrentAttributeOptions();
-        
-        return (
-            <div>
-                <Input type="search" ref="search"
+        const attributeOptions = this.getCurrentAttributeOptions();
+        const suggestOptions = this.state.suggestions.map(sugg => {
+            return <option key={sugg} value={sugg}
+                           disabled={this.props.attributes.indexOf(sugg) != -1}>
+                       {sugg}
+                   </option>;
+        });
+        const attributeFilter = (
+                <FormControl type="search" ref="search"
                     value={this.state.pattern}
                     onChange={this.onPatternChange.bind(this)}
                     placeholder="e.g */vac/*/pressure"/>
-                <Overlay show={this.state.suggestions.length > 0}
-                         placement="bottom" container={this} rootClose={true}
-                         target={() => findDOMNode(this.refs.search)}>
-                {this.makeSuggestions()}
-                </Overlay>
-
-
-                <Input type="select" ref="attributes" id="current-attributes"
-                    multiple={true} value={this.state.selectedAttributes}
-                    style={{width: "100%", height: "200"}}
-                    onChange={this.onSelectAttributes.bind(this)}>
-                     {attributeOptions}
-                  </Input>
-
-                {this.renderAttributeInfo()}
-            
-                <Button onClick={this.onRemoveAttributes.bind(this)}
-                    disabled={this.state.selectedAttributes.length == 0}>
-                  Remove
-                </Button>
+        );
+        
+        return (
+                <div>
+                  <form>               
+                    <Panel header={attributeFilter}>
+                      <FormGroup>
+                        <FormControl componentClass="select" ref="attributes"  
+                                     multiple value={this.state.selectedSuggestions}
+                                     style={{width: "100%", height: "150"}}
+                                     onChange={this.onSelectSuggestions.bind(this)}>
+                          {suggestOptions}
+                        </FormControl>
+                      </FormGroup>                
+                      <DropdownButton bsStyle="success" title="Add"
+                                      disabled={this.state.selectedSuggestions.length == 0}>
+                        <MenuItem eventKey={0}
+                                  onSelect={this.onAddAttributes.bind(this)}>Left Y</MenuItem>
+                        <MenuItem eventKey={1}
+                                  onSelect={this.onAddAttributes.bind(this)}>Right Y</MenuItem>
+                      </DropdownButton>
+                    </Panel>
                 
-            </div>                
+                    <FormGroup>
+                      <FormControl componentClass="select" ref="attributes" id="current-attributes"
+                                   multiple={true} value={this.state.selectedAttributes}
+                                   style={{width: "100%", height: "200"}}
+                                   onChange={this.onSelectAttributes.bind(this)}>
+                        {attributeOptions}
+                      </FormControl>
+                    </FormGroup>
+                    <FormGroup>                
+
+            
+                      <Button bsStyle="danger" onClick={this.onRemoveAttributes.bind(this)}
+                              disabled={this.state.selectedAttributes.length == 0}
+                              title="Remove the currently selected attribute(s) from the plot">
+                        Remove
+                      </Button>
+                    </FormGroup>
+                    {this.renderAttributeInfo()}
+                  </form>            
+                </div>
         );
     }
     
