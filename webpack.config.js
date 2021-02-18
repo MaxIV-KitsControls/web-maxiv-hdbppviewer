9f7ff22a32eacd808573740ad5b0e2d5b3a51afd
Muhammad Saad <13bscsmusaad@seecs.edu.pk>
2021-02-18 10:33:02 +0100

    formatted webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,45 @@
 const path = require('path');
 
 module.exports = {
-    entry: "./js/main.js",
-    output: {
-        path: path.resolve(__dirname, 'static'),
-        publicPath: '/static/',
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: [{
-                loader: "babel-loader",
-                options: {
-                  presets: [
-                    "es2015",
-                    "stage-0", "stage-1", "stage-2", "stage-3",
-                    "react"
-                  ]
-                }
-              }]
-            },
-            {
-              test: /\.css$/,
-              use: ["style-loader", "css-loader"]
-            }
-          ]
-    },
-    resolve: {
-        extensions: [ ".js" ]
-    },
-    externals: {
-        "d3": "d3"
-    },
-    devServer: {
-      contentBase: './static',
-      proxy: {
-        '/': 'http://localhost:5005'
+  entry: "./js/main.js",
+  output: {
+    path: path.resolve(__dirname, 'static'),
+    publicPath: '/static/',
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "es2015",
+              "stage-0", "stage-1", "stage-2", "stage-3",
+              "react"
+            ]
+          }
+        }]
       },
-      watchContentBase: true
-    }
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js"]
+  },
+  externals: {
+    "d3": "d3"
+  },
+  devServer: {
+    contentBase: './static',
+    proxy: {
+      '/': 'http://localhost:5005'
+    },
+    watchContentBase: true
+  }
 };
